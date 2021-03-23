ca13f62a817f2951a46bb9b6fed5053025499c48
Oscar Norman <oscar.norman95@gmail.com>
2021-03-23 14:23:55 +0100

    layout semi done

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 $(function () {
 
+    let storageArray = [];
     load();
 
     function load() {
@@ -15,16 +16,31 @@ $(function () {
     
         json.forEach((product) => {
             output += `
-            <div id = "product" class = "col-4">
-            <img src="${product.image}" alt="Product image" class = "img-fluid">
+            <div id = "product" class = "col-3 border border-dark">
+            <p id = "productTitle">
             ${product.title}
+            </p>
+            <img src="${product.image}" alt="Product image" class = "img-fluid" style="width: 200px; height: 200px">
+            <p id = "productDescription">
+            ${product.description}
+            </p>
+            <p id = "productPrice">
+            ${product.price}$
+            </p>
+            <div class = "p-2">
+            <button class="btn btn-primary" id="addToCartBtn">
+            Add
+            </button>
+            </div>
             </div>
             `
         });
-    
         $(".row").prepend(output);
     }
 
-
+    $(document).on("click", "#addToCartBtn", function() {
+        let item = $(this).
+        storageArray.push
+    })
 });
 
