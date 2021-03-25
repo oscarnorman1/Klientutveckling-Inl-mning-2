5045e756cb76d72a055f32905d6b4dd0cc60fcd7
Oscar Norman <oscar.norman95@gmail.com>
2021-03-25 20:48:23 +0100

    localStorage done

--- JS/cart.js
+++ JS/cart.js
@@ -5,9 +5,10 @@ let totalCost = new Number();
 loadStorage();
 
 function loadStorage() {
-    let data = localStorage.getItem("array");
+    let data = localStorage.getItem("cart");
     if(data !=null) {
         storageArray = JSON.parse(data);
+        console.log(storageArray);
     }
 
     if(storageArray.length > 0) {
@@ -15,19 +16,35 @@ function loadStorage() {
             $("#tbodyTable").prepend(
                 `
                 <tr>
-                    <td>
-                        <div class = "row">
-                        <div class ="col-1" 
-                        </div>
-                    </td>
+                <th scope="row">
+                    <img src="${storageArray[i].productImage}" alt="" id="cartImage">
+                </th>
+                <td>
+                    ${storageArray[i].productTitle}
+                </td>
+                <td>
+                    <div class="number-input md-number-input">
+                        <button class="btn btn-secondary" id="removeOneProduct">-</button>
+                        <input min="1" name="quantity" value="${storageArray[i].productQuantity}" type="text" readonly="" id="cartQuantity">
+                        <button class="btn btn-secondary" id="addOneProduct">+</button>
+                      </div>
+                </td>
+                <td>
+                    $${storageArray[i].productPrice}
+                </td>
+                <td>
+                    <button class = "btn btn-warning p-1">Remove</button>
+                </td>
                 </tr>
-                    
-
                 `
             )
         }
     }
 }
+
+$(document).on("click", "#remove-localStorage", function() {
+    localStorage.clear();
+})
 
 
 
--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,38 @@
 $(function () {
+  let productArray = [];
+  let cartArray = new Array();
+  load();
+  loadCart();
 
-    let productArray = [];
-    let storageArray = [];
-    load();
+  function load() {
+    fetch("https://webacademy.se/fakestore/")
+      .then((resp) => resp.json())
+      .then(function (data) {
+        render(data);
+        loadArray(data);
+      })
+      .catch((err) => console.error(err));
+  }
 
-    function load() {
-        fetch("https://webacademy.se/fakestore/")
-            .then(resp => resp.json())
-            .then(function (data) {
-                render(data);
-                loadArray(data);
-            })
-            .catch(err => console.error(err));
-    }
-    
-    function loadArray(json) {
-        json.forEach((product) => {
-            let prod = {
-                productID: product.id,
-                productTitle: product.title,
-                productDescription: product.description,
-                productImage: product.image,
-                productPrice: product.price,
-                productCategory: product.category
-            }
-            productArray.push(prod);
-        });
-        console.log(productArray[0]);
-    }
+  function loadArray(json) {
+    json.forEach((product) => {
+      let prod = {
+        productID: product.id,
+        productTitle: product.title,
+        productDescription: product.description,
+        productImage: product.image,
+        productPrice: product.price,
+        productCategory: product.category,
+        productQuantity: 1,
+      };
+      productArray.push(prod);
+    });
+  }
 
-    function render(json) {
-        console.log(json);
-        let output = "";
-        json.forEach((product) => {
-            output += `
+  function render(json) {
+    let output = "";
+    json.forEach((product) => {
+      output += `
             <div id = "product" class = "col-4 border border-dark">
             <div id = "productID" style = "display: none;">
             ${product.id}
@@ -54,18 +53,45 @@ $(function () {
             </button>
             </div>
             </div>
-            `
-        });
-        $(".row").prepend(output);
+            `;
+    });
+    $(".row").prepend(output);
+  }
+
+  function loadCart() {
+    let data = localStorage.getItem("cart");
+    if (data != null) {
+      cartArray = JSON.parse(data);
+      console.log(cartArray);
     }
+  }
 
-    $(document).on("click", "#addToCartBtn", function() {
-        for(let i = 0; i < productArray.length; i++) {
-            if(productArray[i].productID == $(this).closest("#product").find("#productID").text().trim()) {
-                storageArray.push(productArray[i]);
-                localStorage.setItem("cart", JSON.stringify(storageArray));
+  $(document).on("click", "#addToCartBtn", function () {
+    for (let i = 0; i < productArray.length; i++) {
+      if (productArray[i].productID == $(this).closest("#product").find("#productID").text().trim()) {
+        
+        console.log(cartArray);
+        if (cartArray.length == 0 || cartArray == undefined) {
+          console.log("La in objekt i cart när cartArray length är 0");
+          cartArray.push(productArray[i]);
+          localStorage.setItem("cart", JSON.stringify(cartArray));
+          return;
+        } else {
+          for (let j = 0; j < cartArray.length; j++) {
+            for (let k = 0; k < productArray.length; k++) {
+              if (productArray[i].productID == cartArray[j].productID) {
+                console.log("Hittade duplett! Ökade quantity med 1");
+                cartArray[j].productQuantity++;
+                localStorage.setItem("cart", JSON.stringify(cartArray));
+                return;
+              }
             }
+          }
+          console.log("Hittade ej duplett! Lade in produkt i cartArray");
+          cartArray.push(productArray[i]);
+          localStorage.setItem("cart", JSON.stringify(cartArray));
         }
-    })
+      }
+    }
+  });
 });
-
